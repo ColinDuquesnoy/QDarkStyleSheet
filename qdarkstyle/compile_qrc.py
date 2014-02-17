4fe9a9a81a0ab8bcd0278c81eff275939b1a5aa0
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-02-17 15:56:10 +0100

    Rm compilation for Qt:
    c++ users will compile it themselves as described in the readme

--- qdarkstyle/compile_qrc.py
+++ qdarkstyle/compile_qrc.py
@@ -40,8 +40,8 @@ def compile_all():
     """
     Compile style.qrc using rcc, pyside-rcc and pyrcc4
     """
-    print("Compiling for Qt: style.qrc -> style.rcc")
-    os.system("rcc style.qrc -o style.rcc")
+    # print("Compiling for Qt: style.qrc -> style.rcc")
+    # os.system("rcc style.qrc -o style.rcc")
     print("Compiling for PyQt4: style.qrc -> pyqt_style_rc.py")
     os.system("pyrcc4 -py3 style.qrc -o pyqt_style_rc.py")
     print("Compiling for PySide: style.qrc -> pyside_style_rc.py")
