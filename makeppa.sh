1b2dc633db328ff31e46612e90bf5a03db328114
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-04-20 09:19:12 +0200

    Update ppa script to build package for trusty, utopic and vivid

--- makeppa.sh
+++ makeppa.sh
@@ -4,24 +4,42 @@
 ppa="ppa:colin-duquesnoy/experimental"
 name="qdarkstyle"
 version="1.16"
-debian_version=1
 
 # read pgp key from gpg_key file
 gpg_key=`cat gpg_key`
 
 # generate debian source package and .orig.tar.gz
-python3 setup.py --command-packages=stdeb.command sdist_dsc --suite trusty --debian-version ${debian_version}
+python3 setup.py --command-packages=stdeb.command sdist_dsc
 
-# sign our package and prepare it for ppa upload
-pushd deb_dist
-dpkg-source -x ${name}_${version}-${debian_version}.dsc
-pushd ${name}-${version}
-debuild -S -sa -k${gpg_key}
-popd
+date=`date -R`
 
-# upload to ppa
-dput ${ppa} *.changes
-popd
+# clean pyc files
+find . -name "*.pyc" -exec rm -rf {} \;
+
+
+for suite in 'trusty' 'utopic' 'vivid'
+do
+    # sign our package and prepare it for ppa upload
+    pushd deb_dist
+    pushd ${name}-${version}
+    
+    # update changelog to include ubuntu release
+    changelog="${name} (${version}-1ppa1~${suite}1) ${suite}; urgency=low
+  * Initial release
+ -- Colin Duquesnoy <colin.duquesnoy@gmail.com>  ${date}
+"
+    echo "$changelog" > debian/changelog
+    cat debian/changelog
+
+    debuild -S -sa -k${gpg_key}
+    popd
+
+    # upload to ppa
+    dput ${ppa} *.changes      
+    rm -rf *.dsc *.changes
+
+    popd
+done
 
 # cleanup
 rm -rf *.tar.gz deb_dist/ dist/
