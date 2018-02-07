e7de70f7a8e9f4a27ba7ee344de8ae888c8a8b9a
Daniel Pizetta <daniel.pizetta@usp.br>
2018-02-07 16:50:08 -0200

    Remove license, update python version, update description

--- setup.py
+++ setup.py
@@ -1,33 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# The MIT License (MIT)
-#
-# Copyright (c) <2013-2014> <Colin Duquesnoy>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-"""
-QDarkStyle is a darks stylesheet for python qt applications
-"""
+
 from setuptools import setup, find_packages
 from qdarkstyle import __version__
+from qdarkstyle import __doc__ as long_desc
 
 setup(
     name='QDarkStyle',
@@ -37,32 +13,21 @@ setup(
     license='MIT',
     author='Colin Duquesnoy',
     author_email='colin.duquesnoy@gmail.com',
-    description='A dark stylesheet for PyQt/PySide applications',
-    long_description="""
-This package provides a dark style sheet for PySide/PyQt4/PyQt5 applications.
-
-All you have to do is the following::
-
-    import qdarkstyle
-    app = QtGui.QApplication().instance()
-    # PySide
-    app.setStyleSheet(qdarkstyle.load_stylesheet())
-    # PyQt4
-    app.setStyleSheet(qdarkstyle.load_stylesheet(pyside=False))
-    # PyQt5
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-
-""",
+    description='A dark stylesheet for Python and Qt applications',
+    long_description=long_desc,
     classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: X11 Applications :: Qt',
-          'Environment :: Win32 (MS Windows)',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX :: Linux',
-          'Operating System :: MacOS',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Topic :: Software Development :: Libraries :: Application Frameworks'])
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: X11 Applications :: Qt',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Application Frameworks'
+    ])
