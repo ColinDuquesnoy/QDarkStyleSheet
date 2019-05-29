af7ad63a65e9cdcc654a07ccdab36635fdb93953
dpizetta <daniel.pizetta@gmail.com>
2019-05-29 14:52:07 -0300

    Better logger info, preparing to check results

--- qdarkstyle/utils/images.py
+++ qdarkstyle/utils/images.py
@@ -6,8 +6,9 @@ Utilities to process and convert svg images to png using palette colors.
 
 # Standard library imports
 from __future__ import absolute_import, division, print_function
+
+import logging
 import os
-import sys
 import tempfile
 
 # Third party imports
@@ -16,10 +17,12 @@ from qtpy.QtGui import QIcon
 from qtpy.QtWidgets import QApplication
 
 # Local imports
-from qdarkstyle import _logger, RC_PATH, REPO_PATH, SVG_PATH, IMAGES_PATH
+from qdarkstyle import IMAGES_PATH, RC_PATH, SVG_PATH
 from qdarkstyle.palette import DarkPalette
 
 IMAGE_BLACKLIST = ['base_palette']
+
+_logger = logging.getLogger(__name__)
 
 
 def _get_file_color_map(fname, palette):
@@ -28,6 +31,7 @@ def _get_file_color_map(fname, palette):
     color_focus = palette.COLOR_SELECTION_LIGHT
     color_pressed = palette.COLOR_SELECTION_NORMAL
     color_normal = palette.COLOR_FOREGROUND_DARK
+
     name, ext = fname.split('.')
 
     files_map = {
@@ -38,6 +42,7 @@ def _get_file_color_map(fname, palette):
             name + '_pressed.' + ext: color_pressed,
         }
     }
+
     for f, file_colors in files_map.items():
         if f == fname:
             break
@@ -72,15 +77,16 @@ def create_palette_image(base_svg_path=SVG_PATH, path=IMAGES_PATH,
                          palette=DarkPalette):
     """Create palette image svg and png image on specified path."""
     # Needed to use QPixmap
-    app = QApplication([])
+    _ = QApplication([])
 
     base_palette_svg_path = os.path.join(base_svg_path, 'base_palette.svg')
     palette_svg_path = os.path.join(path, 'palette.svg')
     palette_png_path = os.path.join(path, 'palette.png')
 
-    _logger().info("Creating palette image ...")
-    _logger().info("From : %s" % palette_svg_path)
-    _logger().info("To   : %s" % palette_png_path)
+    _logger.info("Creating palette image ...")
+    _logger.info("Base: %s" % base_palette_svg_path)
+    _logger.info("From: %s" % palette_svg_path)
+    _logger.info("To  : %s" % palette_png_path)
 
     with open(base_palette_svg_path, 'r') as fh:
         data = fh.read()
@@ -115,29 +121,48 @@ def create_images(base_svg_path=SVG_PATH, rc_path=RC_PATH,
         64: '@2x.png',
     }
 
-    _logger().info("Creating images ...")
-    _logger().info("From : %s" % base_svg_path)
-    _logger().info("Temp : %s" % temp_dir)
-    _logger().info("To   : %s" % rc_path)
+    _logger.info("Creating images ...")
+    _logger.info("SVG folder: %s" % base_svg_path)
+    _logger.info("TMP folder: %s" % temp_dir)
+    _logger.info("PNG folder: %s" % rc_path)
+
+    num_svg = len(svg_fnames)
+    num_png = 0
+    num_ignored = 0
 
     for height, ext in heights.items():
+
         for svg_fname in svg_fnames:
             svg_name = svg_fname.split('.')[0]
-            if svg_name not in IMAGE_BLACKLIST:
-                _logger().info("Working on: %s" % svg_fname)
 
+            if svg_name not in IMAGE_BLACKLIST:
                 svg_path = os.path.join(base_svg_path, svg_fname)
                 color_files = _get_file_color_map(svg_fname, palette=palette)
 
+                _logger.debug(" Working: %s"
+                                % os.path.basename(svg_path))
+
                 for color_svg_name, color in color_files.items():
                     temp_svg_path = os.path.join(temp_dir, color_svg_name)
-
-                    _logger().debug("Temporary file: %s" % temp_svg_path)
-
                     _create_colored_svg(svg_path, temp_svg_path, color)
+
+                    _logger.debug("  Temporary: %s"
+                                    % os.path.basename(temp_svg_path))
+
                     width = height
                     png_fname = color_svg_name.replace('.svg', ext)
                     png_path = os.path.join(rc_path, png_fname)
+                    num_png += 1
+
                     convert_svg_to_png(temp_svg_path, png_path, height, width)
 
-                    _logger().debug("Path: %s" % png_path)
+                    _logger.debug("  Creating: %s"
+                                    % os.path.basename(png_path))
+            else:
+                num_ignored += 1
+                _logger.debug(" Ignored blacklist: %s"
+                                % os.path.basename(svg_path))
+
+    _logger.info("# SVG files: %s" % num_svg)
+    _logger.info("# PNG files: %s" % num_png)
+    _logger.info("# SVG ignored: %s" % num_ignored)
