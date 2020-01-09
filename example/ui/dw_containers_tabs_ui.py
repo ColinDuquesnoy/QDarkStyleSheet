e2fbccce42a54ef11034427c286aa2b264bfdf91
dpizetta <daniel.pizetta@gmail.com>
2020-01-09 15:38:53 -0300

    UPD: update sources

--- example/ui/dw_containers_tabs_ui.py
+++ example/ui/dw_containers_tabs_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dw_containers_tabs.ui'
 #
-# Created by: PyQt5 UI code generator 5.13.0
+# Created by: PyQt5 UI code generator 5.14.1
 #
 # WARNING! All changes made in this file will be lost!
 
