b70b89f887ce9763e1ebec877f15b9635268490f
Daniel Cosmo Pizetta <daniel@adialante.com>
2023-11-09 17:17:29 -0300

    Add COLOR-DISABLED, fixes #337

--- qdarkstyle/palette.py
+++ qdarkstyle/palette.py
@@ -68,6 +68,7 @@ class Palette(object):
             'COLOR_ACCENT_2',
             'COLOR_ACCENT_3',
             'COLOR_ACCENT_4',
+            'COLOR_DISABLED',
             'OPACITY_TOOLTIP',
             'SIZE_BORDER_RADIUS',
             'BORDER_1',
